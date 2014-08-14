2cec1f61d73dbcdf63f7c58b04cb16a5b2b43c41
hsld21x <hsld21x@student.cs.uwaterloo.ca>
2014-08-14 10:35:16 -0400

    removed extra outputs

--- helloworld.java
+++ helloworld.java
@@ -2,10 +2,6 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         System.out.println("Hello, World");
-	System.out.println("Hey, yo!");
-	System.out.println("new branch now");
-
-	System.out.println("bad");
 
     }
 
